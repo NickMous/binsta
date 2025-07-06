ee97457300b5580bd25814175c5a2396ec852751
Sander den Hollander <sander@webenjoyer.com>
2025-07-06 14:22:46 +0200

    Add dynamic component loading again

--- src/Resources/main.ts
+++ src/Resources/main.ts
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
+import { createApp, type Component } from 'vue'
 import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+const components: Record<string, Component> = {}
+const modules = import.meta.glob('./components/*.vue', { eager: true }) as Record<string, { default: Component }>
+for (const path in modules) {
+    const match = path.match(/\.\/components\/(.*)\.vue$/)
+    if (match) {
+        const componentName = match[1]
+        components[componentName] = modules[path].default
+    }
+}
+
+let el = document.getElementById('app');
+
+if (!el) {
+    throw new Error('No element with id "app" found in the DOM');
+}
+
+createApp({
+    template: el.innerHTML,
+    components
+}).mount(el)
